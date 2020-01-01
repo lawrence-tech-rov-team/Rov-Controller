ed6a75778ccfcafb70d4bd110d1196f2a9c3eaad
zcareyltu <zcarey@ltu.edu>
2019-12-31 19:11:35 -0500

    Empty Project

---
+++ RovController/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * RovController.cpp
+ *
+ * Created: 12/31/2019 6:51:24 PM
+ * Author : zcarey
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
